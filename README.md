47d8f23d13e3005176e90b6f76e5814b36234ba7
Waldek <reclikmail@gmail.com>
2020-05-24 17:02:02 +0200

    Baza danych

--- README.md
+++ README.md
@@ -28,3 +28,42 @@ php bin/console doctrine:query:sql "SHOW DATABASES"
 ```
 Jeśli nie zwróci errora to śmiga :)
 
+##### Utworzenie Encji User
+> Encja to zmapowana tabela db na obiekt (Klasa encji to nazwa tabeli, zmienne to nazwy kolumn tabeli)  
+> np:  
+> Mam tabelę User z polami id, username, password, itp.  
+> Wtedy każde pole tabeli to obiekt, więc do username odwołuję się jak do obiektu:  
+> aby pobrać - getUsername()  
+> aby przypisać wartość - setUsername()
+
+1. Tworzę Encję użytkownika w CLI (jako unique property ustawiam username)
+```
+php bin/console make:user
+```
+
+2. w pliku <b>config/packages/security.yaml</b> komentuję `provider: app_user_provider`  
+
+3. Aby rozszerzyć encję, dodać do istniejącej encji nowe pole wykonuję:
+```
+php bin/console make:entity
+```
+Wpisuję nazwę już istniejącej encji User - dzięki temu będę mógł dodać kolejne pola.
+
+##### Utworzenie Encji Photo
+1. Dodaję encję o nazwie Photo
+```
+php bin/console make:entity
+```
+
+
+##### Migracja bazy danych
+
+1. Tworzę plik migracji
+```
+php bin/console make:migration
+```
+
+2. Wysyłam migrację do MySql
+```
+php bin/console doctrine:migrations:migrate
+```
---
+++ src/Migrations/Version20200524145045.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200524145045 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE photo (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, filename VARCHAR(211) NOT NULL, uploaded_at DATETIME NOT NULL, is_public TINYINT(1) NOT NULL, INDEX IDX_14B78418A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(210) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE photo ADD CONSTRAINT FK_14B78418A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE photo DROP FOREIGN KEY FK_14B78418A76ED395');
+        $this->addSql('DROP TABLE photo');
+        $this->addSql('DROP TABLE user');
+    }
+}
